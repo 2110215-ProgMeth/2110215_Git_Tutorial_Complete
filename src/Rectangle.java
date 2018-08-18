11a580a291ad0531f186bb6ddc705b3a61b76b98
Pakapon Kaewmark <sleep191@gmail.com>
2018-08-18 18:52:08 +0700

    :sparkles: Add Rectangle class

---
+++ src/Rectangle.java
@@ -0,0 +1,14 @@
+
+public class Rectangle {
+	
+	private int width;
+	private int height;
+	
+	public Rectangle(int width, int height) {
+		super();
+		this.width = width;
+		this.height = height;
+	}
+	
+	
+}
